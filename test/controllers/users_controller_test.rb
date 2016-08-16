aaee47ae64edf9cb72666faff4f6228e22deb11f
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-08-16 17:45:49 +0900

    users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,12 @@ class UsersControllerTest < ActionController::TestCase
   
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
   end
   
   test "should get new" do
@@ -23,4 +29,19 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
+  test "should redirecte edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: {
+      name: @user.name, email: @user.email
+    }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
