ff2d7fd3217bcfab72430c7214b9b18bf192d7bc
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-08-16 09:39:22 +0900

    user login integration test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,4 +4,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test "login with invalid information" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, session: { email: "", password: "" }
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+  
 end
