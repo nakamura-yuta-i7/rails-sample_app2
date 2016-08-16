3caa0a1a275231591b0bbc3ef802c134c10276a1
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-08-16 20:20:14 +0900

    user activate, seed, fixtures, accessor, migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  
+  attr_accessor :remember_token, :activation_token
+  
+  # before_save { self.email = email.downcase }
+  before_save   :downcase_email
+  
+  before_create :create_activation_digest
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  has_secure_password
-  # validates :password, presence: true, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  
+  has_secure_password
   
   # 永続的セッションで使用するユーザーをデータベースに記憶する
   def remember
@@ -42,5 +48,18 @@ class User < ActiveRecord::Base
     end
   
   end
+  
+  private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
   
 end
