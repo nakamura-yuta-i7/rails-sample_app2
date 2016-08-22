f35fd59f364a36572e413bf0d0459bf91b433de7
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-08-22 23:12:56 +0900

    いろいろ

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,19 +1,31 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  
+  # アップロード可能な拡張子のリスト
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
